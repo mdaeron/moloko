3d2f6224c96e5772d709d992e712d7f50bc5a967
mdaeron <mathieu@daeron.fr>
2025-11-27 01:01:06 +0100

    Change path of app

--- src/moloko/__init__.py
+++ src/moloko/__init__.py
@@ -2,4 +2,4 @@ import subprocess
 
 def main() -> None:
     print('Hello from moloko!')
-    subprocess.run('uv run streamlit run src/moloko/app.py'.split())
+    subprocess.run('uv run streamlit run moloko/src/moloko/app.py'.split())
