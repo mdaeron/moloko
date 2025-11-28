e7c87b785adc02374a4be56051814fa2e0877137
mdaeron <mathieu@daeron.fr>
2025-11-28 08:27:47 +0100

    subprocess runs streamlit instead of uv run streamlit

--- src/moloko/__init__.py
+++ src/moloko/__init__.py
@@ -4,8 +4,6 @@ import subprocess
 def main() -> None:
     print(f'Hello from moloko at {os.path.abspath(os.path.dirname(__file__))}!')
     subprocess.run([
-#     	'uv',
-#     	'run',
     	'streamlit',
     	'run',
     	os.path.abspath(os.path.dirname(__file__)) + '/app.py',
